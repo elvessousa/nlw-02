f126cb7f3c6a96761f158133d881c851183e7696
Elves Sousa <elves.sousa@gmail.com>
2020-08-07 18:32:38 -0300

    Day 3.

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,10 +8,15 @@ import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
   children?: React.ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  description,
+  children,
+}) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -23,6 +28,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
         {children}
       </div>
     </header>
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,36 +2,51 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-interface TeacherItemProps {
-  text?: string;
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: string;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ text }) => {
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", { user_id: teacher.id });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="http://lorempixel.com/400/400" alt="Foto" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Nome do professor</strong>
-          <span>Máteria</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas quaerat,
-        cumque voluptas saepe dolores corrupti quidem ipsum, earum porro
-        exercitationem iste omnis tempora aliquid ad fugit fuga? Voluptate,
-        itaque. Commodi?
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
-          Preço/hora <strong>R$ 80,00</strong>
+          Preço/hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="WhatsApp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
